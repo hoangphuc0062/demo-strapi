d608cda16d542b0ec63bcc31c994c11fbc1ee24e
Hoang Phuc <phucndh@stsbmt.com>
2025-07-02 10:00:51 +0700

    fix plugin to deloy

--- strapi-cms/config/plugins.ts
+++ strapi-cms/config/plugins.ts
@@ -1,1 +1,18 @@
-export default () => ({});
+export default () => ({
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        landingPage: true,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+          introspection: true,
+          // Không cần playground ở đây nữa vì đã có landingPage
+        },
+      },
+    },
+  });
+  
